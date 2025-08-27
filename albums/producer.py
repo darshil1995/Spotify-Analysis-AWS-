87fb9181d9b6f01a988a19f58524c06bd3fff787
Darshil Shah <darshil321995@gmail.com>
2025-08-27 09:24:10 -0400

    Update producer.py

--- albums/producer.py
+++ albums/producer.py
@@ -1,13 +1,17 @@
 import pandas as pd
 from kafka import KafkaProducer
 import json
+import boto3, json
+
+client = boto3.client("secretsmanager", region_name="us-east-1")
+secret = json.loads(client.get_secret_value(SecretId="kafka/producer")["SecretString"])
 
 producer = KafkaProducer(
     bootstrap_servers="d2m7vo4hu0bcm3tve3sg.any.us-east-1.mpx.prd.cloud.redpanda.com:9092",
   security_protocol="SASL_SSL",
   sasl_mechanism="SCRAM-SHA-256",
-  sasl_plain_username="darshilshah",
-  sasl_plain_password="4a4HL3lMWY202piQVhBjiIneE3x7U4",
+  sasl_plain_username=secret["username"],
+    sasl_plain_password=secret["password"],
   value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
@@ -25,3 +29,4 @@ for record in albums.to_dict(orient='records'):
 producer.flush()  # ensure all messages are sent
 producer.close()
 print("All records sent successfully!")
+
